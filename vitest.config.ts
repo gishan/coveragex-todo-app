8ab0d65ecd9b76077d10b2b7c6cee1a4dcbf9922
Gishan Abeysinghe <gishan.abeysinghe@gmail.com>
2025-01-17 17:28:43 +0530

    fix testing for frontend

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
   test: {
     environment: 'jsdom',
+    setupFiles: ['./setup.ts'],
     globals: true,
     include: ['packages/backend/src/**/*.test.ts', 'packages/frontend/src/**/*.test.tsx'],
   },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './packages/frontend/src')
+    }
+  }
 });
