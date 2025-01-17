c9185f930269a4fa2b8eeff3c4984bc7a5b256d9
Gishan Abeysinghe <gishan.abeysinghe@gmail.com>
2025-01-17 10:45:59 +0530

    add docker compose setting

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -10,4 +10,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: true, // Listen on all network interfaces
+    port: 5173,
+  },
 })
